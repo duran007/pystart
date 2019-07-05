855ff0141ec0cc5ff8aad9883f322a6883d3fc83
sov <vsov007@gmail.com>
2019-07-06 00:05:47 +0300

    Last exercis is 5_1d

---
+++ exercises/05_basic_scripts/answer_5_1c.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+london_co = {
+    'r1': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.1'
+    },
+    'r2': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.2'
+    },
+    'sw1': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '3850',
+        'ios': '3.6.XE',
+        'ip': '10.255.0.101',
+        'vlans': '10,20,30',
+        'routing': True
+    }
+}
+
+device = input('Print device name (r1, r2 or sw1): ')
+option = input('Print device option (' + ', '.join(london_co[device].keys()) + '): ')
+print('-'*30)
+print(london_co[device].get(option, 'Option not found'))
